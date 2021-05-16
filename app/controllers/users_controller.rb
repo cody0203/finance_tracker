124a38b039c329a93461bfd370855b23e09c3e99
Pham Cong Dinh <congdinh2395@gmail.com>
2021-05-16 16:00:50 +0700

    Implement user searching, view friend profile and follow their stocks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
   def my_portfolio
     @followed_stocks = current_user.stocks
   end
+
+  def show
+    @user = User.find(params[:id])
+    @followed_stocks = @user.stocks
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
-  get 'search_stock', to: "stocks#search"
-  resources :friendships, only: [:index, :destroy, :create]
+  get 'search_stock', to: 'stocks#search'
+  resources :friendships, only: %i[index destroy create]
+  get 'search_friend', to: 'friendships#search'
+  resources :users, only: [:show]
 end
